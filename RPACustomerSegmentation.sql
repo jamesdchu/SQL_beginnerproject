fa27f579f931f87ca8022e3ed929b20da415d8d5
James Chu <jamesdchu@gmail.com>
2020-08-08 16:45:03 -0400

    RPA Customer Segmentation Querying

---
+++ RPACustomerSegmentation.sql
@@ -0,0 +1,13 @@
+--  select * from users limit 20;
+
+-- select email, birthday from users where birthday like "198%";
+
+-- select email, created_at from users where created_at < "2017-05-01";
+
+-- select email from users where test = 'bears';
+
+-- select email from users where campaign like "BBB%";
+
+-- select email from users where campaign like "%2";
+
+-- select email from users where campaign not Null and test not Null;
